2cbcc5ccad8b72510d75016458a2844d38294d3d
Abhilash-P <abhilash@billerq.com>
2025-04-18 13:17:03 +0530

    files created

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("C U S T O M P A I N T"),
+      ),
+      body: const SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
