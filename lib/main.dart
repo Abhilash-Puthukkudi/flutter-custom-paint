4bfc75e455a4fc3e606a8d1a628ed2c38b89603c
Abhilash-Puthukkudi <Abhilashp44@gmail.com>
2025-04-18 13:58:41 +0530

    drawing a horizontal line

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -26,13 +31,39 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         title: const Text("C U S T O M  - P A I N T"),
       ),
-      body: const SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          children: [],
+      body: Center(
+        child: Container(
+          color: Colors.grey[200],
+          child: CustomPaint(
+            painter: MyPainter(),
+            size: const Size(300, 400),
+          ),
         ),
       ),
     );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint();
+    paint.color = Colors.red;
+    paint.strokeWidth = 20;
+
+    paint.strokeCap = StrokeCap.round;
+    
+
+    /// DRAWING HORIZONTAL LINE STARTING FROM ORIGIN [offet.zero] and taking the full width [offset(size.width,0)]
+
+// [offset.zero (0,0)]
+// offset(xaxis point, y axis point)
+    canvas.drawLine(Offset.zero, Offset(size.width, 0), paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return false;
   }
 }
